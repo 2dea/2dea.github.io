52fe0f3de22e3c4b14d1af77bb0b1bc09a2d7511
2dea <greatego@gmail.com>
2025-05-09 17:52:57 +0900

    Ksa IBK-BWS-Proto (soo)

---
+++ Works/ksas/ibk-bws-proto/src/app/pages/bss/BSSTI0201M.tsx
@@ -0,0 +1,314 @@
+/**
+ * @description 영업지원 > 연수원 > 연수원품목입출고등록
+ */
+
+// dependency
+import React, { useRef, useState } from 'react';
+
+// components
+// import '@modules/Index';
+import Icon from 'app/shared/modules/OIcon';
+import Label from 'app/shared/modules/OLabel';
+import CommonButton from 'app/shared/modules/OButton';
+import ImageButton from 'app/shared/modules/OImageButton';
+import XDropdown from 'app/shared/modules/XDropdown';
+import XMultiSelect from 'app/shared/modules/XMultiSelect';
+import { Dialog } from 'primereact/dialog';
+import { OverlayPanel } from 'primereact/overlaypanel';
+import { Tooltip } from 'primereact/tooltip';
+import { MenuItem } from 'primereact/menuitem';
+import { BreadCrumb } from 'primereact/breadcrumb';
+import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
+import { AutoComplete } from 'primereact/autocomplete';
+import { addLocale } from 'primereact/api';
+import { Nullable } from 'primereact/ts-helpers';
+import { Calendar } from 'primereact/calendar';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { RadioButton, RadioButtonChangeEvent } from 'primereact/radiobutton';
+import { Checkbox } from 'primereact/checkbox';
+import { DataTable } from 'primereact/datatable';
+import { Splitter, SplitterPanel } from 'primereact/splitter';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+function BSSTI0201M() {
+  // Dialog
+  // const [visible, setVisible] = useState<boolean>(true);
+
+  // OverlayPanel
+  const viewimageOverlay0 = useRef(null);
+  const viewimageOverlay1 = useRef(null);
+  const viewimageOverlay2 = useRef(null);
+
+  // BreadCrumb
+  const paths: MenuItem[] = [{ label: '홈' }, { label: '영업지원' }, { label: '연수원' }, { label: '연수원품목입출고등록' }];
+
+  // InputText
+  const [value, setValue] = useState<string>('');
+
+  // InputNumber
+  const [InputNumberValue, setInputNumberValue] = useState(0);
+
+  // AutoComplete
+  const [AutoCompleteValue, setAutoCompleteValue] = useState('');
+  const [AutoCompleteItems, setAutoCompleteItems] = useState([]);
+  const AutoCompleteSearch = (evt) => {
+    setAutoCompleteItems([...Array(10).keys()].map(item => evt.query + '-' + item));
+  };
+  const itemTemplate = (item) => {
+    return (
+      <>
+        <span className="p-autocomplete-cell">[0001] 홍길동</span>
+        <span className="p-autocomplete-cell sep">|</span>
+        <span className="p-autocomplete-cell">부장</span>
+        <span className="p-autocomplete-cell sep">|</span>
+        <span className="p-autocomplete-cell">[0031] 을지6가</span>
+        <span className="p-autocomplete-cell sep">|</span>
+        <span className="p-autocomplete-cell">0031</span>
+        <span className="p-autocomplete-cell sep">|</span>
+        <span className="p-autocomplete-cell">[1111] 을지6가</span>
+      </>
+    );
+  };
+
+  // Calendar
+  addLocale('ko', {
+    firstDayOfWeek: 0,
+    dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+    monthNames: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  });
+  const [Date, setDate] = useState<Nullable<Date>>(null);
+  const [Time, setTime] = useState(null);
+
+  // RadioButton
+  const [ingredient, setIngredient] = useState<string>('');
+
+  // Checkbox
+  const [checked, setChecked] = useState<boolean>(false);
+
+  return (
+    <>
+      <div className="roles" data-div-role="0">
+        <div className="div-header">
+          <div className="main">
+            <BreadCrumb model={paths} className="o-breadcrumb" aria-label="Breadcurmb trail" />
+
+            <div className="m-title">
+              <h1 className="o-heading level1">
+                <span className="label">연수원품목입출고등록</span>
+
+                <label className="o-check type-symbol style-favorite">
+                  <input type="checkbox" title="마이메뉴 등록" value="" className="bind" /><i aria-hidden="true"></i>
+                  <span className="label _hidden">마이메뉴 등록</span>
+                </label>
+              </h1>
+            </div>
+          </div>
+
+          <div className="binds">
+            <div className="m-binds type-start">
+              <div className="group _assistive">
+                <CommonButton label="도움말" icon="help" className="_normal" />
+              </div>
+            </div>
+
+            <div className="m-binds type-end">
+              <div className="group _primary">
+                <CommonButton label="저장" className="_solid-primary" />
+                </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <!-- /* Contents { @DEV } --> */}
+        <div className="div-contents">
+          <div className="o-grid">
+            <div className="column">
+
+              <form className="m-filter-form">
+              <div className="fieldset">
+
+                <div className="o-field colspan-2">
+                  <Label label={`구분`} require={false} />
+                  <div className="fields">
+                    <div className="m-checks">
+                      <label className="o-check"><RadioButton inputId="" name="RADIO_01" value="RADIO_01-1" onChange={(e) => setIngredient(e.value)} checked={true || ingredient === 'RADIO_01-1'} /><span className="label">충주연수원</span></label>
+                      <label className="o-check"><RadioButton inputId="" name="RADIO_01" value="RADIO_01-2" onChange={(e) => setIngredient(e.value)} checked={ingredient === 'RADIO_01-2'} /><span className="label">기흥연수원</span></label>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="o-field colspan-2">
+                  <Label label={`상태`} require={false} />
+                  <div className="fields">
+                    <div className="m-checks">
+                      <label className="o-check"><RadioButton inputId="" name="RADIO_01" value="RADIO_01-1" onChange={(e) => setIngredient(e.value)} checked={true || ingredient === 'RADIO_01-1'} /><span className="label">전체</span></label>
+                      <label className="o-check"><RadioButton inputId="" name="RADIO_01" value="RADIO_01-2" onChange={(e) => setIngredient(e.value)} checked={ingredient === 'RADIO_01-2'} /><span className="label">비품</span></label>
+                      <label className="o-check"><RadioButton inputId="" name="RADIO_01" value="RADIO_01-2" onChange={(e) => setIngredient(e.value)} checked={ingredient === 'RADIO_01-2'} /><span className="label">소모품</span></label>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="o-field">
+                  <Label label={`품목명`} require={false} />
+                  <div className="fields">
+                    <div className="o-form _input">
+                      <InputText placeholder="" value={value} className="bind" onChange={(e) => setValue(e.target.value)} />
+                      <i aria-hidden="true"></i>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+                <div className="binds">
+                  <CommonButton label="조회" className="_inquire" />
+                </div>
+              </form>
+
+              <div className="o-section">
+                <div className="m-header">
+                  <h2 className="o-heading level2"><span className="label">관리품목내역</span></h2>
+                </div>
+
+                <div className="main _primary">
+                  <DataTable className="o-grid-table g-hide" />{/* Unused { @DEV } */}
+                  <div className="o-grid-table p-datatable">
+                    <div className="table-container p-datatable-wrapper">
+                      <table className="p-datatable-table p-datatable-scrollable-table">
+                        <colgroup>
+                          <col className="wdth-10" />
+                          <col className="wdth-10" />
+                          <col />
+                          <col />
+                          <col />
+                          <col />
+                          <col />
+                          <col />
+                          <col />
+                          <col />
+                          <col />
+                          <col />
+                          <col />
+                          <col />
+                          <col />
+                        </colgroup>
+
+                        <thead className="p-datatable-thead">
+                          <tr>
+                            <th className="p-align-center"><div className="p-column-header-content"><label className="o-check"><Checkbox checked={checked} onChange={e => setChecked(e.checked)} /><span className="label _hidden">전체&#x200B;선택</span></label></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">순번</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">연수원</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">구분</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">품목번호</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">품목명</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">규격</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">단위</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">단위당개수</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">입출고구분</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">입출고수량</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">담당자</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">확인자</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">등록일자</span></div></th>
+                            <th className="p-align-center"><div className="p-column-header-content"><span className="p-column-title">비고</span></div></th>
+                          </tr>
+                        </thead>
+
+                        <tbody className="p-datatable-tbody">
+                          <tr className="p-datatable-emptymessage">
+                            <td colSpan={15}>등록된 데이터가 없습니다.</td>
+                          </tr>
+                        </tbody>
+
+                        <tbody className="p-datatable-tbody">
+                        {[...Array(24)].map((e, idx) => (
+                          <tr key={idx} className={`${idx === 2 ? 'p-x-mode-edited' : idx === 0 ? 'p-highlight' : ''}`}>{/* 수정된 행 tr.p-x-mode-edited | 그리드 최초 진입시 첫번째 행 tr.p-highlight <$tr.trigger('click')> { @DEV } */}
+                            {/* <td><Checkbox checked={checked} onChange={e => setChecked(e.checked)} /></td> */}
+                            <td><label className="o-check"><Checkbox checked={checked} onChange={e => setChecked(e.checked)} /><span className="label _hidden">선택</span></label></td>
+                            <td>{idx === 2 ? <Icon icon="grid-edited" /> : idx + 1}</td>{/* 수정된 행 .o-icon._grid-edited 순번 대체 { @DEV } */}
+                            <td>충주연수원</td>
+                            <td>소모품</td>
+                            <td>0003</td>
+                            <td className="g-start">린스(휴양동)</td>
+                            <td className="g-end">450ML</td>
+                            <td>1박스</td>
+                            <td className="g-end">1</td>
+                            <td>
+                              <div className="o-field">
+                                <div className="fields">
+                                  <div className="o-form _select wdth-30">
+                                    <XDropdown appendTo={document.body} className="bind" />
+                                    <i aria-hidden="true"></i>
+                                  </div>
+                                </div>
+                              </div>
+                            </td>
+                            <td>
+                              <div className="o-field">
+                                <div className="fields">
+                                  <div className="o-form _input wdth-20 g-end">
+                                    <InputText placeholder="" value="0" className="bind" onChange={(e) => setValue(e.target.value)} />
+                                    <i aria-hidden="true"></i>
+                                  </div>
+                                </div>
+                              </div>
+                            </td>
+                            <td>
+                              <div className="o-field">
+                                <div className="fields">
+                                  <div className="o-form _input wdth-20">
+                                    <InputText placeholder="" value={value} className="bind" onChange={(e) => setValue(e.target.value)} />
+                                    <i aria-hidden="true"></i>
+                                  </div>
+                                </div>
+                              </div>
+                            </td>
+                            <td>
+                              <div className="o-field">
+                                <div className="fields">
+                                  <div className="o-form _input wdth-20">
+                                    <InputText placeholder="" value={value} className="bind" onChange={(e) => setValue(e.target.value)} />
+                                    <i aria-hidden="true"></i>
+                                  </div>
+                                </div>
+                              </div>
+                            </td>
+                            <td colSpan={1}>
+                              <div className="o-field">
+                                <div className="fields">
+                                  <div className="o-form _input type-date wdth-50">
+                                    <Calendar placeholder="" value={Date} locale="ko" dateFormat="yy-mm-dd" mask="9999-99-99" appendTo={document.body} className="bind" onChange={(e) => setDate(e.value)} showIcon icon={<Icon icon="calendar" />} />
+                                    <i aria-hidden="true"></i>
+                                  </div>
+                                </div>
+                              </div>
+                            </td>
+                            <td>
+                              <div className="o-field">
+                                <div className="fields">
+                                  <div className="o-form _input">
+                                    <InputText placeholder="" value={value} className="bind" onChange={(e) => setValue(e.target.value)} />
+                                    <i aria-hidden="true"></i>
+                                  </div>
+                                </div>
+                              </div>
+                            </td>
+                          </tr>
+                        ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        {/* <!-- // Contents { @DEV } --> */}
+      </div>
+    </>
+  );
+}
+
+export default BSSTI0201M;
