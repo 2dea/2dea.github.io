1fbad7f7218b123c5553337c18ce66c611c6aa6f
2dea <greatego@gmail.com>
2025-07-31 10:43:37 +0900

    Ksa IBK-BWS-Proto :: Main

---
+++ Works/ksas/ibk-bws-proto/src/app/pages/cmm/CMM000301M.tsx
@@ -0,0 +1,108 @@
+/**
+ * @description 공통 ~ 메인
+ */
+
+// dependency
+import React, { useRef, useState } from 'react';
+
+// components
+// import '@modules/Index';
+import FavoriteDiv from 'app/shared/layouts/Favorite';
+import ConsultsDiv from 'app/shared/layouts/Consults';
+import Icon from 'app/shared/modules/OIcon';
+import Label from 'app/shared/modules/OLabel';
+import CommonButton from 'app/shared/modules/OButton';
+import ImageButton from 'app/shared/modules/OImageButton';
+import InputCheck from 'app/shared/modules/OInputCheck';
+import InputRadio from 'app/shared/modules/OInputRadio';
+import XDropdown from 'app/shared/modules/XDropdown';
+import XMultiSelect from 'app/shared/modules/XMultiSelect';
+import { Dialog } from 'primereact/dialog';
+import { OverlayPanel } from 'primereact/overlaypanel';
+import { Tooltip } from 'primereact/tooltip';
+import { MenuItem } from 'primereact/menuitem';
+import { BreadCrumb } from 'primereact/breadcrumb';
+import { InputText } from 'primereact/inputtext';
+import { InputMask } from 'primereact/inputmask';
+import { InputNumber } from 'primereact/inputnumber';
+import { AutoComplete } from 'primereact/autocomplete';
+import { addLocale } from 'primereact/api';
+import { Nullable } from 'primereact/ts-helpers';
+import { Calendar } from 'primereact/calendar';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { RadioButton, RadioButtonChangeEvent } from 'primereact/radiobutton';
+import { Checkbox } from 'primereact/checkbox';
+import { DataTable } from 'primereact/datatable';
+import { Splitter, SplitterPanel } from 'primereact/splitter';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+function CMM000301M() {
+  // Dialog
+  // const [visible, setVisible] = useState<boolean>(true);
+
+  // OverlayPanel
+  const viewimageOverlay0 = useRef(null);
+  const viewimageOverlay1 = useRef(null);
+  const viewimageOverlay2 = useRef(null);
+
+  // BreadCrumb
+  const paths: MenuItem[] = [{ label: '홈' }, { label: '행내등기·우편물' }, { label: '우편물' }, { label: '우편물신청' }];
+
+  // InputText
+  const [value, setValue] = useState<string>('');
+
+  // InputMask
+  const [Mask, setMask] = useState(null);
+
+  // InputNumber
+  const [InputNumberValue, setInputNumberValue] = useState(0);
+
+  // AutoComplete
+  const [AutoCompleteValue, setAutoCompleteValue] = useState('');
+  const [AutoCompleteItems, setAutoCompleteItems] = useState([]);
+  const AutoCompleteSearch = (evt) => {
+    setAutoCompleteItems([...Array(10).keys()].map(item => evt.query + '-' + item));
+  };
+  const itemTemplate = (item) => {
+    return (
+      <>
+        <span className="p-autocomplete-cell">[0001] 홍길동</span>
+        <span className="p-autocomplete-cell sep">|</span>
+        <span className="p-autocomplete-cell">부장</span>
+        <span className="p-autocomplete-cell sep">|</span>
+        <span className="p-autocomplete-cell">[0031] 을지6가</span>
+        <span className="p-autocomplete-cell sep">|</span>
+        <span className="p-autocomplete-cell">0031</span>
+        <span className="p-autocomplete-cell sep">|</span>
+        <span className="p-autocomplete-cell">[1111] 을지6가</span>
+      </>
+    );
+  };
+
+  // Calendar
+  addLocale('ko', {
+    firstDayOfWeek: 0,
+    dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+    monthNames: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  });
+  const [Date, setDate] = useState<Nullable<Date>>(null);
+  const [Time, setTime] = useState(null);
+
+  // RadioButton
+  const [ingredient, setIngredient] = useState<string>('');
+
+  // Checkbox
+  const [checked, setChecked] = useState<boolean>(false);
+
+  return (
+    <>
+      <div className="roles page-index" data-div-role="0" style={{ background: 'var(--color-ground-70)' }}>
+        <div className="div-temp" style={{  }}>
+          <img src={require("assets/images/temp/CMM000301M_010.png")} alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CMM000301M;
